285cb55b13253132ce9f73abfa2ad8db2a317810
Nolan Caine <nolancaine@gmail.com>
2021-08-28 21:45:28 -0600

    Finish methods

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,18 +3,8 @@ import Recipe from './Recipe';
 class RecipeRepository {
   constructor(recipeData) {
     this.recipes = recipeData;
-    // this.recipes = recipeData.map(
-    //   (recipe) =>
-    //     new Recipe(
-    //       recipe.id,
-    //       recipe.image,
-    //       recipe.ingredients,
-    //       recipe.instructions,
-    //       recipe.name,
-    //       recipe.tags
-    //     )
-    // )
   }
+
   makeRecipes() {
     this.recipes = this.recipes.map(
       (recipe) =>
@@ -28,8 +18,15 @@ class RecipeRepository {
         )
     )
   }
+
   returnCriteria(property, criterion) {
     return this.recipes.filter(recipe => recipe[property].includes(criterion));
+  }
+
+  returnRecipesByIngredient(ingredientId) {
+    return this.recipes.filter(recipe =>
+      recipe.ingredients.some(ingredient => ingredient.id === ingredientId)  
+    );
   }
 }
 
