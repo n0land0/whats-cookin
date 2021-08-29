7e2358b2f1db6121acb5bdf4034e7ba4e244db81
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-08-29 19:46:12 -0400

    Add recipe method of returning ingredients names and total cost

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,5 +1,5 @@
-import recipeData from '../data/recipes';
-import ingredientsData from '../data/ingredients';
+import recipeData from "../data/recipes";
+import ingredientsData from "../data/ingredients";
 
 class Recipe {
   constructor(id, image, ingredients, instructions, name, tags) {
@@ -14,14 +14,40 @@ class Recipe {
   showIngredientsByName() {
     let idArray = this.ingredients.map((ele) => ele.id);
 
-    let nameArray = idArray.map(
-      (id) => ingredientsData.find((data) => data.id === id).name
-    );
+    let nameArray = idArray.map((id) => ingredientsData.find((data) => data.id === id).name);
 
     return nameArray;
   }
 
-  // calculateRecipeCost()
+  calculateRecipeCost() {
+    let idArray = this.ingredients.map((ele) => ele.id);
+    /*
+    let amountArray = this.ingredients.map((ele) => ele.quantity.amount);
+
+    let costArray = idArray.map((id) => ingredientsData.find((data) => data.id === id).estimatedCostInCents);
+
+    console.log(idArray, amountArray, costArray);
+
+    return costArray
+      .map((costInCents, index) => costInCents * amountArray[index])
+      .reduce((accumulator, current) => {
+        accumulator += current;
+        return accumulator;
+      });
+      */
+    let ingredientArray = idArray.map((id) => ingredientsData.find((data) => data.id === id));
+    console.log(typeof ingredientArray[0].estimatedCostInCents);
+    console.log(typeof this.ingredients[0].quantity.amount);
+
+    return ingredientArray.reduce(
+      (acc, ingredient) =>
+        (acc +=
+          ingredient.estimatedCostInCents * this.ingredients.find((data) => data.id === ingredient.id).quantity.amount),
+      0
+    );
+  }
+
+  // (ingredient.quantity.amount * costArray) / 100
 
   // showInstructions()
   // return obj with step # as key, instruction text as value?
@@ -29,3 +55,19 @@ class Recipe {
 }
 
 export default Recipe;
+
+// let idArray = this.ingredients.map((ele) => ele.id);
+
+// let amountArray = this.ingredients.map((ele) => ele.quantity.amount)
+
+// let costArray = idArray.map((id) =>
+// ingredientsData.find((data) =>
+// data.id === id
+// ).estimatedCostInCents);
+
+// costArray.map((costInCents, index) =>
+// costInCents * amountArray[index]
+// ).reduce((accumulator, current) => {
+// accumulator += current;
+// return accumulator;
+// })
