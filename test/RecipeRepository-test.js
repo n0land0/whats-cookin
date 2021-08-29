d5bc84fa32bed5167c56defe6a8c36c7c6cd9edb
Nolan Caine <nolancaine@gmail.com>
2021-08-28 21:46:22 -0600

    Write ingredient test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,14 +1,15 @@
 import { assert } from 'chai';
+
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
+import Ingredient from '../src/classes/Ingredient';
+
 import recipeData from '../src/data/recipes';
 
 describe('RecipeRepository', () => {
-  // let recipe;
   let recipeRepository;
 
   beforeEach(function () {
-    // recipe = new Recipe();
     recipeRepository = new RecipeRepository(recipeData);
   });
   it('Should be a function', () => {
@@ -31,15 +32,22 @@ describe('RecipeRepository', () => {
 
   it('Should be able to return a list of recipes based on tags', () => {
     let snacks = recipeRepository.returnCriteria('tags', 'snack'); 
-    console.log(snacks)
-    // console.log(recipeRepository.returnCriteria('tags', 'snack'));
-    // console.log(recipeRepository.recipes[0]['tags'].includes('snack'));
-    // console.log(recipeRepository.recipes[0]['tags']);
 
-    assert.includeMembers(snacks[0].tags, ['snack'])
+    assert.includeMembers(snacks[0].tags, ['snack']);
   });
     
-  it('Should be able to return a list of recipes based on name');
+  it('Should be able to return a list of recipes based on name', () => {
+    let dirtySteve = recipeRepository.returnCriteria('name', 'Dirty Steve'); 
 
-  it('Should be able to return a list of recipes based on ingredients');
+    assert.equal(dirtySteve[0].name.includes('Dirty Steve'), true);
+  }); 
+
+  it('Should be able to return a list of recipes based on ingredients', () => {
+    let wheatFlour = new Ingredient(20081, "wheat flour", 142)
+    let glutens = recipeRepository.returnRecipesByIngredient(wheatFlour.id);
+
+    for (var i = 0; i < glutens.length; i++) {
+      assert.equal(glutens[i].ingredients.find(ingredient => ingredient.id === 20081).id, wheatFlour.id);
+    }
+  });
 });
