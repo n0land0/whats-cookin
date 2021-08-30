218ac6757c3efc5d9e655fb1f6d89b0b90536587
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-08-30 18:28:46 -0400

    Show all recipe btn works now

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,28 @@
 import "./styles.css";
 import apiCalls from "./apiCalls";
-import RecipeRepository from "../src/classes/RecipeRepository";
-import recipeData from "../src/data/recipes.js";
+import RecipeRepository from "./classes/RecipeRepository";
+import recipeData from "./data/recipes.js";
 
 const showAllRecipeBtn = document.getElementById("show-all-recipes");
 const showRecipeByTagBtn = document.getElementById("show-recipe-by-tag");
 const searchRecipeByNameBtn = document.getElementById("search-recipe-by-name");
+const recipeContainer = document.getElementById("recipe-container");
+var recipePool;
 
-console.log("Hello world");
+window.addEventListener("load", generateRecipes);
+showAllRecipeBtn.addEventListener("click", showAllRecipes);
 
-let recipeRepository = new RecipeRepository(recipeData);
-recipeRepository.makeRecipes();
+function generateRecipes() {
+  event.preventDefault();
+  let recipeRepository = new RecipeRepository(recipeData);
+  recipeRepository.makeRecipes();
+  recipePool = recipeRepository.recipes;
+  //   console.log(recipePool);
+}
+
+// console.log(recipePool);
+
+function showAllRecipes() {
+  console.log(recipePool);
+  // appendChild to recipe container - card for each recipe w/ pic & name
+}
