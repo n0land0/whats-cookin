78726cc97689b3390ec18e0525159144eeb91709
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-08-30 17:05:32 -0400

    Add three buttons to html. Add background color

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,13 @@
-import './styles.css';
-import apiCalls from './apiCalls';
-import RecipeRepository from '../src/classes/RecipeRepository';
+import "./styles.css";
+import apiCalls from "./apiCalls";
+// import RecipeRepository from "../src/classes/RecipeRepository";
+// import recipeData from "../src/data/recipes.js";
 
-console.log('Hello world');
+const showAllRecipeBtn = document.getElementById("show-all-recipes");
+const showRecipeByTagBtn = document.getElementById("show-recipe-by-tag");
+const searchRecipeByNameBtn = document.getElementById("search-recipe-by-name");
+
+console.log("Hello world");
+
+let recipeRepository = new RecipeRepository(recipeData);
+recipeRepository.makeRecipes();
