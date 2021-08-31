72fe0aa7d055a4b9ab06a1ec9064d0177b14167b
Nolan Caine <nolancaine@gmail.com>
2021-08-30 19:13:41 -0600

    Write tests

---
+++ test/IngredientRepository-test.js
@@ -0,0 +1,34 @@
+import { assert } from 'chai';
+
+import IngredientRepository from '../src/classes/IngredientRepository';
+import Ingredient from '../src/classes/Ingredient';
+import ingredientsData from '../src/data/ingredients';
+
+describe('IngredientRepository', () => {
+  let ingredientRepository;
+
+  beforeEach(function () {
+    ingredientRepository = new IngredientRepository(ingredientsData);
+  });
+  
+  it('Should be a function', () => {
+    assert.isFunction(IngredientRepository);
+  });
+
+  it('Should be an instance of IngredientRepository', () => {
+    assert.instanceOf(ingredientRepository, IngredientRepository);
+  });
+
+  it('Should hold ingredient objects', () => {
+    ingredientRepository.makeIngredients();
+
+    assert.instanceOf(ingredientRepository.ingredients[0], Ingredient);
+  });
+
+  it('Should be able to use ingredient name to return ingredient ID', () => {
+    let name = ingredientRepository.getIngredientId('wheat flour');
+
+    assert.equal(name, 20081)
+  });
+
+});
